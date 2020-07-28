24605f737acbca07e395815ff591dbda7d902a10
anshgit101 <ansh.khandelwal@students.iiit.ac.in>
2020-07-29 01:27:42 +0400

    admin settings

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Ansh Khandelwal',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'July 27,2020'
-    },
-    {
-        'author': 'Kunal Tawde',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'July 28,2020'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html',context)
 
